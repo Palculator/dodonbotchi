e5e96ae374bad8e2515e32596f4f8601aeef1584
Signaltonsalat <37553150+Signaltonsalat@users.noreply.github.com>
2018-05-06 00:01:04 +0200

    Add util module that contains commonly-used functions

---
+++ dodonbotchi/util.py
@@ -0,0 +1,69 @@
+"""
+Module containing various helper functions used throughout DoDonBotchi.
+"""
+import os
+import os.path
+
+from datetime import datetime
+
+SERIAL_LETTERS = 'DPRAEWZ5JNI7LB2UQCHKSOFGMVX3TY'
+YEAR_BASE = 2018
+
+
+def ensure_directories(*dirs):
+    """
+    Goes through each directory in the given list of paths and creates it and
+    any super directories needed if required.
+    """
+    for directory in dirs:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+
+def get_now_string():
+    """
+    Returns the current time in roughly the ISO8601 format, but with : replaced
+    with - to avoid file sytem problems and returns it.
+    """
+    now = datetime.now()
+    now = now.isoformat()
+    now = now.replace(':', '-')
+    now = now[0:now.rfind('.')]
+    return now
+
+
+def generate_serial_number(num):
+    """
+    Generates a sci-fi-mecha-like serial number for the given integer and
+    returns it.
+    """
+    model_num = '{:03}'.format(num % 1000)
+    num *= 10
+
+    model_type = ''
+    while num >= len(SERIAL_LETTERS):
+        model_type += SERIAL_LETTERS[num % len(SERIAL_LETTERS)]
+        num //= len(SERIAL_LETTERS)
+    model_type += SERIAL_LETTERS[num]
+
+    if len(model_type) > 5:
+        model_prefix = model_type[0] + model_type[1].lower()
+        model_type = model_prefix + '-' + model_type[2:]
+
+    return model_type + '-' + model_num
+
+
+def generate_now_serial_number():
+    """
+    Generates a sci-fi-mecha-like serial number based on the current date and
+    time and returns it.
+    """
+    now = datetime.now()
+
+    num = 0
+    num += now.hour
+    num += now.day * 24
+    num += now.month * 31 * 24
+    num += (now.year - YEAR_BASE) * 12 * 31 * 24
+
+    return generate_serial_number(num)
